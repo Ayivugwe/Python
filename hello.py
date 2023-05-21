c4c987efd1e937504354588f6b7ac1a3479b886e
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-05-21 19:29:25 +0200

    Added the hello file

---
+++ hello.py
@@ -0,0 +1,11 @@
+#This code is for practicing the basics of Python
+
+print("Hello, World!")
+def add(a,b):
+ return a + b
+
+print("Hello, World! The sum is %s" % add(1,5))
+
+name = input("What is your name? \n")
+print("Welcome Mr. "+name)
+print("The length of your name is: "+str(len(name)))
