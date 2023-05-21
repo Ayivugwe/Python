d68faf3777ad9e4726116a0951a5a9832b1012c8
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2023-05-21 19:36:08 +0200

    Adding expanatory comments

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 #This code is for practicing the basics of Python
 
+#This code print the Hello World! message
 print("Hello, World!")
+
+# This is a method declaration in Python that calcultes the sum of two numbers and prints their sum 
 def add(a,b):
  return a + b
 
 print("Hello, World! The sum is %s" % add(1,5))
 
+#This is the code to ask for user input in Python
 name = input("What is your name? \n")
-print("Welcome Mr. "+name)
-print("The length of your name is: "+str(len(name)))
+print("Welcome Mr. "+name) #This prints the name entered by the user
+print("The length of your name is: "+str(len(name))) # This prints the length of the name entered by the user
+
